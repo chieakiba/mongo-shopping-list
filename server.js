03dccefac7a0cdb0d12c7639de6e496b276d0586
Chie <akiba.chie@gmail.com>
2016-07-12 16:40:23 -0700

    Initial commit after updating and testing

---
+++ server.js
@@ -0,0 +1,116 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var config = require('./config');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+var runServer = function (callback) {
+    mongoose.connect(config.DATABASE_URL, function (err) {
+        if (err) {
+            return callback(err);
+        }
+
+        app.listen(config.PORT, function () {
+            console.log('Listenting on localhost:' + config.PORT);
+            if (callback) {
+                callback();
+            }
+        });
+    });
+};
+
+if (require.main === module) {
+    runServer();
+};
+
+var Item = require('./models/item');
+
+app.get('/items', function (req, res) {
+    Item.find(function (err, items) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            });
+        }
+        res.json(items);
+    });
+});
+
+app.get('/items/:id', function (req, res) {
+    Item.findById({
+        _id: req.params.id
+    }, function (err, item) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            });
+        }
+        res.json(item);
+    });
+});
+
+
+
+
+app.post('/items', function (req, res) {
+    Item.create({
+            name: req.body.name
+        },
+        function (err, item) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Internal Server Error'
+                });
+            }
+            res.status(201).json(item);
+        }
+    );
+});
+
+
+// single item -> :id
+app.put('/items/:id', function (req, res) {
+    var id = req.params.id;
+    Item.findByIdAndUpdate({
+            _id: id
+        }, {
+            name: req.body.name
+        },
+        function (err, item) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Internal Server Error'
+                });
+            }
+            res.status(201).json(item);
+        });
+});
+
+app.delete('/items/:id', function (req, res) {
+    //Hint:  id from params?
+    var id = req.params.id;
+    Item.findOneAndRemove({
+        _id: id
+    }, function (err, item) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            });
+        }
+        res.status(201).json(item);
+    });
+});
+
+app.use('*', function (req, res) {
+    res.status(404).json({
+        message: 'Not Found'
+    });
+});
+
+exports.app = app;
+exports.runServer = runServer;
---
+++ test/test-server.js
@@ -0,0 +1,119 @@
+global.DATABASE_URL = 'mongodb://localhost/shopping-list-test';
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+
+var server = require('../server.js');
+var Item = require('../models/item');
+
+var should = chai.should();
+var app = server.app;
+
+chai.use(chaiHttp);
+
+describe('Shopping List', function () {
+    before(function (done) {
+        server.runServer(function () {
+            Item.create({
+                name: 'Broad beans'
+            }, {
+                name: 'Tomatoes'
+            }, {
+                name: 'Peppers'
+            }, function () {
+                done();
+            });
+
+        });
+    });
+
+    it('should list items on GET', function (done) {
+        chai.request(app)
+            .get('/items')
+            .end(function (err, res) {
+                should.equal(err, null);
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.should.have.length(3);
+                res.body[0].should.be.a('object');
+                res.body[0].should.have.property('_id');
+                res.body[0].should.have.property('name');
+                res.body[0].name.should.be.a('string');
+                res.body[0].name.should.be.eq('Broad beans');
+                res.body[1].name.should.be.eq('Tomatoes');
+                res.body[2].name.should.be.eq('Peppers');
+                done();
+            });
+    });
+
+    it('should add an item on POST', function (done) {
+        chai.request(app)
+            .post('/items')
+            .send({
+                'name': 'Kale'
+            })
+            .end(function (err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.name.should.be.a('string');
+                res.body.name.should.equal('Kale');
+                done();
+            });
+    });
+
+    it('should edit an item on PUT', function (done) {
+        chai.request(app)
+            .get('/items')
+            .end(function (err, res) {
+                chai.request(app)
+                    .put('/items/' + res.body[0]._id)
+                    .send({
+                        name: "Rice"
+                    })
+                    .end(function (err, res) {
+                        // res.body [inspect]
+                        should.equal(err, null);
+                        res.should.have.status(201);
+                        res.should.be.json;
+                        chai.request(app)
+                            .get('/items/' + res.body._id).end(function (err, res) {
+                                res.body.should.be.a('object');
+                                res.body.should.have.property('name');
+                                res.body.should.have.property('_id');
+                                res.body.name.should.be.a('string');
+                                res.body.name.should.be.eq('Rice');
+                                done();
+                            })
+                    });
+            })
+    });
+
+    it('should delete an item on DELETE', function (done) {
+        chai.request(app)
+            .get('/items')
+            .end(function (err, res) {
+                chai.request(app)
+                    .delete('/items/' + res.body[0]._id)
+                    .end(function (err, res) {
+                        should.equal(err, null);
+                        res.should.have.status(201);
+                        res.should.be.json;
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('name');
+                        res.body.should.have.property('_id');
+                        res.body.name.should.be.a('string');
+                        done();
+                    });
+            })
+    });
+
+    after(function (done) {
+        Item.remove(function () {
+            done();
+        });
+    });
+});
