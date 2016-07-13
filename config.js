6a49a25ef4367e5d681abecbc8f84a3cbf45d1d3
Chie <akiba.chie@gmail.com>
2016-07-13 16:07:07 -0700

    Updated the production configuration point to my mLab database

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    process.env.NODE_ENV === 'production' ?
-    'mongodb://localhost/shopping-list' :
-    'mongodb://localhost/shopping-list-dev';
+    process.env.NODE_ENV === 'production'
+'mongodb://localhost/shopping-list':
+'mongodb://chieakiba:Etoile921mlab@ds015892.mlab.com:15892/mongo-shopping-list';
 exports.PORT = process.env.PORT || 8080;
