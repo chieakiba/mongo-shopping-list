bc046224c259318cc66b08c0e905d37d8e920dd9
Chie <akiba.chie@gmail.com>
2016-07-14 10:11:58 -0700

    Updated production link

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
+console.log("WHAT IS NODENV?", process.env.NODE_ENV);
+
+
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    process.env.NODE_ENV === 'production'
-'mongodb://chieakiba:Etoile921mlab@ds015892.mlab.com:15892/mongo-shopping-list';
+    process.env.NODE_ENV === 'production' ?
+    'mongodb://localhost/shopping-list' :
+    'mongodb://localhost/shopping-list-dev';
 exports.PORT = process.env.PORT || 8080;
